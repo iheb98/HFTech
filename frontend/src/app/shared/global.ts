986f9f3e79cace0ed22783ee96677e4e7ff9dded
IHeb <ihebferjani04@gmail.com>
2023-03-08 21:50:08 +0100

    initializing project

--- frontend/src/app/shared/global.ts
+++ frontend/src/app/shared/global.ts
@@ -1,6 +1,8 @@
+import { environment } from "src/environments/environment";
+
 export const GlobalVariable = Object.freeze({
     IMAGE_BASE_PATH: 'https://stock-mbh-backend.herokuapp.com/uploads/',
-    BASE_PATH: 'http://localhost:3000',
+    BASE_PATH: environment.production? '' : 'http://localhost:3000',
     STATES: ["Neuf", "Bon occasion", "Occasion"],
     MONTHES : ['Janvier', 'Février', 'Mars', 'Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 });
